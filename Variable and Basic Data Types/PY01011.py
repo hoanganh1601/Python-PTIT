9acadbe22296f216ff54340cb16da0253ed50707
HoangAnh <hoanganh1612006@gmail.com>
2026-08-19 13:48:20 +0700

    LIỆT KÊ SỐ ĐẸP

--- Variable and Basic Data Types/PY01011.py
+++ Variable and Basic Data Types/PY01011.py
@@ -1,3 +1,26 @@
+validNumbers = []
+def init():
+    queue = ['2', '4', '6', '8']   
+    
+    while queue:
+        half = queue.pop(0)
+        palidrome = half + half[::-1]
+        validNumbers.append(int(palidrome))
+        
+        if(len(half) < 3):
+            for nums in ['0', '2', '4', '6', '8']:
+                queue.append(half + nums)
+    
+    validNumbers.sort()        
+
+
 if __name__ == '__main__':
+    init()
     test = int(input());
     
+    for _ in range(test):
+        n = int(input())
+        for num in validNumbers:
+            if(num >= n): break
+            print(num, end = " ")
+        print()
