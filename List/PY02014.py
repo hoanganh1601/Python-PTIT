c57096d8cf98ecb446196ad4575969ef35b2bfd5
HoangAnh <hoanganh1612006@gmail.com>
2026-09-14 17:27:39 +0700

    BIẾN ĐỔI NGUYÊN TỐ

--- List/PY02014.py
+++ List/PY02014.py
@@ -1,16 +1,21 @@
 import math
 
-is_prime = [True] * 10001
+LIMIT = 10500
+
+is_prime = [True] * (LIMIT + 1)
 primes = []
 def init():
     is_prime[0] = False
     is_prime[1] = False
-    for i in range(2, math.isqrt(10001)):
+    for i in range(2, math.isqrt(LIMIT) + 1):
         if is_prime[i]:
-            for j in range(i * i, 10001, i):
+            for j in range(i * i, LIMIT + 1, i):
                 is_prime[j] = False
+                
+    for i in range(2, LIMIT + 1):
+        if is_prime[i]:
             primes.append(i)
-    
+
 if __name__ == '__main__':
     init()
     
